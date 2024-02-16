3e093d3a6bccb9bb7b42185b96c47ddd1c3a4470
leonor ramirez <leonor.ramirez@exodus.com.mx>
2024-02-16 12:12:20 -0600

    text to speak done

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, Button } from "react-native";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { BarCodeScanner } from "expo-barcode-scanner";
+//import Tts from "react-native-tts";
+import * as Speech from "expo-speech";
 
 export default function App() {
   const [hasPermission, setHasPermission] = React.useState(false);
-  const [scanData, setScanData] = React.useState();
-  //const [text, setText] = React.useState("Not Scanned");
+  const [text, setText] = useState(""); //el que escaneo
+
+  const [ttsInitialized, setTtsInitialized] = useState(false);
 
   useEffect(() => {
     (async () => {
@@ -15,31 +18,105 @@ export default function App() {
     })();
   }, []);
 
+  useEffect(() => {
+    const initTts = async () => {
+      try {
+        const result = await Tts.init();
+        if (result === "success") {
+          console.log("TTS initialized");
+          setTtsInitialized(true);
+        } else {
+          console.log("Error initializing TTS");
+        }
+      } catch (error) {
+        console.error("Error initializing TTS:", error);
+      }
+    };
+    initTts();
+  }, []);
+
+  const handleTextToSpeech = async () => {
+    console.log("Text to speak:", text);
+
+    try {
+      if (!ttsInitialized) {
+        await initTts();
+      }
+
+      await Tts.speak(text, {
+        androidParams: {
+          KEY_PARAM_PAN: -1,
+          KEY_PARAM_VOLUME: 0.9,
+          KEY_PARAM_STREAM: "STREAM_ALARM",
+        },
+      });
+    } catch (error) {
+      console.error("Error in TTS:", error);
+    }
+  };
+
+  const handleBarCodeScanned = ({ type, data }) => {
+    setText(data);
+    console.log(`Data: ${data}`);
+    console.log(`Type: ${type}`);
+  };
+
   if (!hasPermission) {
     return (
       <View style={styles.container}>
-        <Text>Please grant camera permissions to app.</Text>
+        <Text>Por favor acepte los permisos.</Text>
       </View>
     );
   }
 
-  const handleBarCodeScanned = ({ type, data }) => {
-    setScanData(data);
-    console.log(`Data: ${data}`);
-    console.log(`Type: ${type}`);
-  };
+  let speakHi;
+  if (text != null) {
+    speakHi = () => {
+      const texto = text;
+      const options = {
+        language: "es",
+        pitch: 1,
+        rate: 1,
+        voice: "es-ES-Standard-A",
+      };
+      Speech.speak(texto, options);
+    };
+  } else {
+    speakHi = () => {
+      const texto = "No hay texto";
+      const options = {
+        language: "es",
+        pitch: 1,
+        rate: 1,
+        voice: "es-ES-Standard-A",
+      };
+      Speech.speak(texto, options);
+    };
+  }
 
   return (
     <View style={styles.container}>
       <BarCodeScanner
         style={StyleSheet.absoluteFillObject}
-        onBarCodeScanned={scanData ? undefined : handleBarCodeScanned}
+        onBarCodeScanned={text ? undefined : handleBarCodeScanned}
+        /*Si text tiene un valor, el escaneo de código de barras no activará la función handleBarCodeScanned; 
+        de lo contrario, lo activará. */
       />
 
-      {scanData && (
+      {text && ( //si text tiene un valor truthy se renderiza el siguiente bloque
         <View style={styles.divAlert}>
-          <Text styles={styles.textAlert}>Data: {scanData}</Text>
-          <Button title="Scan Again?" onPress={() => setScanData(undefined)} />
+          <Text style={styles.textAlert}>Data: {text}</Text>
+          <Button title="Scan Again?" onPress={() => setText("")} />
+          {/*AUDIO*/}
+
+          <Button title="Speak" onPress={speakHi} />
+
+          {/* <Button
+            title="Read Scanned Text"
+            onPress={() => handleTextToSpeech()}
+            disabled={!ttsInitialized}
+          /> */}
+          {/*FIN AUDIO*/}
         </View>
       )}
       <StatusBar style="auto" />
@@ -62,6 +139,6 @@ const styles = StyleSheet.create({
   },
   textAlert: {
     fontSize: 20,
-    margin: "10px",
+    margin: 10,
   },
 });
